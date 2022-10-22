976b4a5081bc370735cf5e62fd2efce8201a180d
zxcv-12345 <62590994+zxcv-12345@users.noreply.github.com>
2022-10-23 02:49:09 +0800

    DCNterminaltool-bate1.1

--- 源.cpp
+++ 源.cpp
@@ -2,8 +2,16 @@
 int main()
 {
 	int a,b,c,d;
-	scanf("%d,%d,%d,%d\n",&a,&b,&c,&d);
-	if (a >= 1 && a != 0, b >= 1 && b != 0, c >= 1 && c != 0, d >= 1 && d != 0)
+	scanf_s("%d%d%d%d", &a, &b, &c, &d);
+	if (a >= 255 && b >= 255 && c >= 255 && d >= 255)
+	{
+		printf("输入的IP数值不合法！原因：“输入的IP数值可能是广播号或无效IP数值”");
+	}
+	else if (a <= 1 && b <= 1 && c <= 1 && d <= 1)
+	{
+		printf("输入的IP数值不合法！原因：“输入的IP数值可能是网络号或无效IP数值”");
+	}
+	else if (a >= 1 && b >= 1 && c >= 1 && d >= 1)
 	{
 		printf("输入的IP地址段合法IP地址如下\n");
 		printf("%d.%d.%d.%d\n", a, b, c, d);
@@ -13,3 +21,4 @@ int main()
 		printf("输入的IP数值不合法！");
 	}
 }
+
